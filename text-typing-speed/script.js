927e5a77757c19dd6df83739151108b9b5233f71
s-ferdousy <lopa.ferdousy@gmail.com>
2021-06-24 10:46:35 +0600

    Update script.js

--- text-typing-speed/script.js
+++ text-typing-speed/script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
+const text = 'consectetur, adipisci velit'
 let idx = 1
 let speed = 300 / speedEl.value
 writeText()

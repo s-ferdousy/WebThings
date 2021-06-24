a8a868d5541106ae30da8fa1079e5df108182438
s-ferdousy <lopa.ferdousy@gmail.com>
2021-06-24 10:46:04 +0600

    Update script.js

--- text-typing-speed/script.js
+++ text-typing-speed/script.js
@@ -1,17 +1,18 @@
-const textE1= document.getElementById('text')
-const speedE1= document.getElementById('speed')
-const text= 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...'
-
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'We Love Programming!'
 let idx = 1
-let speed = 300/speedE1.value
-
+let speed = 300 / speedEl.value
 writeText()
-function writeText(){
-    textE1.innerText = text.slice(0, idx)
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
     idx++
-    if(idx>text.length){
+
+    if(idx > text.length) {
         idx = 1
     }
+
     setTimeout(writeText, speed)
 }
 speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
